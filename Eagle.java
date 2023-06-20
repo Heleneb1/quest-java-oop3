06f8d24befa59cfe37323f2e753b521a279e02b8
Helene <helene.blanche@hotmail.fr>
2023-06-20 14:52:48 +0200

    Java08

--- Eagle.java
+++ Eagle.java
@@ -1,4 +1,4 @@
-public class Eagle extends Bird {
+public class Eagle extends Bird implements Fly {
 
     private boolean flying;
     private int altitude;
@@ -21,4 +21,44 @@ public class Eagle extends Bird {
     public String sing() {
         return "Screech!";
     }
+
+    @Override
+    public void takeOff() {
+        if (!this.flying && this.altitude == 0) {
+            this.flying = true;
+            System.out.printf("%s takes off in the sky.%n", this.getName());
+        }
+    }
+
+    @Override
+    public int ascend(int meters) {
+        altitude += meters;
+        System.out.printf("%s flies upward, altitude: %d%n", this.getName(), this.altitude);
+        return altitude;
+    }
+
+    @Override
+    public int descend(int meters) {
+        altitude -= meters;
+        System.out.printf("%s flies upward, altitude: %d%n", this.getName(), this.altitude);
+        return altitude;
+    }
+
+    @Override
+    public void land() {
+        if (altitude == 0 && flying) {
+            flying = false;
+            System.out.printf("%s lands on the ground.%n", this.getName());
+        }
+    }
+
+    @Override
+    public void glide() {
+        if (this.flying && this.altitude == 0) {
+            System.out.printf("%sglides into the air.%n", this.getName());
+        } else {
+            System.out.printf("%s is too high, it can't glide.%n", this.getName());
+        }
+    }
+
 }
